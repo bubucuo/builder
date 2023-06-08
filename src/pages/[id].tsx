8e5ab13445ee7c616e749a06223262445f7d4f7f
bubucuo <276185876@qq.com>
2023-06-08 22:08:33 +0800

    save ls3

---
+++ src/pages/[id].tsx
@@ -0,0 +1,40 @@
+import Canvas from "src/components/Canvas";
+import ClientOnly from "src/components/ClientOnly";
+
+export default function ID({data}: any) {
+  return (
+    <ClientOnly>
+      {data ? (
+        <Canvas canvas={JSON.parse(data.content)} />
+      ) : (
+        <div className="err">
+          id 信息有误，请检查之后重新输入，或者微信联系作者「bubucuo_sy」
+        </div>
+      )}
+    </ClientOnly>
+  );
+}
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+// 此函数在服务端的构建阶段调用，不会在客户端调用，因此这里相当于是直接查询数据库 SSG
+export async function getStaticProps({params}: {params: {id: string}}) {
+  const res = await fetch(
+    "http://builder.codebus.tech/api/web/content/get?id=" + (params.id || 2)
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data: data.result,
+    },
+  };
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import ClientOnly from "src/components/ClientOnly";
-import Canvas from "src/components/Canvas";
 
 export default function Home({data}: any) {
   return (
@@ -13,32 +11,10 @@ export default function Home({data}: any) {
         <meta name="description" content="bubucuo lowcode low code" />
       </Head>
       <main>
-        <ClientOnly>
-          {data ? (
-            <Canvas canvas={JSON.parse(data.content)} />
-          ) : (
-            <div className="err">
-              id 信息有误，请检查之后重新输入，或者微信联系作者「bubucuo_sy」
-            </div>
-          )}
-        </ClientOnly>
+        <div className="err">
+          id 信息有误，请检查之后重新输入，或者微信联系作者「bubucuo_sy」
+        </div>
       </main>
     </>
   );
-}
-
-// SSR，Server-Side Rendering
-// 在页面请求时重新生成HTML发送给客户端。
-// getServerSideProps 只在服务端调用，并不会运行在客户端。
-export async function getServerSideProps({query}: {query: {id: string}}) {
-  const res = await fetch(
-    "http://builder.codebus.tech/api/web/content/get?id=" + (query.id || 2)
-  );
-  const data = await res.json();
-
-  return {
-    props: {
-      data: data.result,
-    },
-  };
 }
